e8dc0047955fc810fdfbe58cf4cb0ef19aa1cbb1
neu5 <neus90@gmail.com>
2017-10-25 21:33:10 +0200

    Show simple text on the screen

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+// import { AppRegistry } from 'react-native';
+// import App from './App';
 
-AppRegistry.registerComponent('albums', () => App);
+// AppRegistry.registerComponent('albums', () => App);
+
+import React from 'react'
+import { Text, AppRegistry } from 'react-native'
+
+const App = () => {
+    return (
+        <Text>Some text</Text>
+    )
+}
+
+AppRegistry.registerComponent('albums', () => App)
